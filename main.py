f22fd7b14b0b36d029ea0a9cc929581021880758
Hen Shalom <xdrakolax@gmail.com>
2018-10-14 22:30:33 +0300

    change to relative path and start adding drop duplication logic

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from writer import main
+from more_itertools import unique_everseen
 
 if __name__ == '__main__':
-    model_location = r'D:\Wrok\dayJob\tableTransform\data\model'
-    tables_location = r'D:\Wrok\dayJob\tableTransform\data\csv'
-    output_location = r'D:\Wrok\dayJob\tableTransform\output'
+    model_location = r'.\data\model'
+    tables_location = r'.\data\csv'
+    output_location = r'.\outputTemp'
+    output_end_location = r'.\output'
     main(model_location, tables_location, output_location)
+    for item in ['edges', 'nodes', 'nodes_header', 'edges_header']:
+        with open(output_location + '\\' + item + '.csv', 'r') as r, open(output_end_location + '\\' + item + '.csv',
+                                                                          'w') as w:
+            w.writelines(unique_everseen(r))
