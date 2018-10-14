30d513321f85d72dd10b1c84bf06efc83dfb9459
Hen Shalom <xdrakolax@gmail.com>
2018-10-14 16:35:21 +0300

    add table context to intilizer

--- modelLoader/intilizer.py
+++ modelLoader/intilizer.py
@@ -5,14 +5,16 @@ from modelLoader.edge import Edge
 
 def data_loader(models_path):
     json_files = [pos_json for pos_json in os.listdir(models_path) if pos_json.endswith('.json')]
-    edges, vertices = [], []
+    models_items = {}
     for file in json_files:
+        edges, vertices = [], []
         with open(os.path.join(models_path, file)) as json_file:
             model = json.load(json_file)
             new_edges, new_vertices = extract_data(model)
             edges += new_edges
             vertices += new_vertices
-    return edges, vertices
+        models_items[file[:-5]] = {"edges": edges, "vertices": vertices}
+    return models_items
 
 
 def extract_data(model):
